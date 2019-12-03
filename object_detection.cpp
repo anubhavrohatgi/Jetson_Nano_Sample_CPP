e7d869f41e3f80152b96679081cb55c3e5ac25bb
Anubhav Rohatgi <anubhav.rohatgi@vnl.in>
2019-12-03 12:41:48 +0530

    bg2

--- object_detection.cpp
+++ object_detection.cpp
@@ -28,7 +28,7 @@ int main( int argc, char** argv ){
 
 	cv::cvtColor(img,img,CV_BGRA2RGBA);
 
-	img.convterTo(img,CV_32FC4, 1/255.0);
+	img.convtertTo(img,CV_32FC4, 1/255.0);
 
 	/*
 	 * create detection network
